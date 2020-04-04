91c4d9ed1e3b215f6cd77e2e87eff3f66ddc060c
AdamSiekierski <adam.siekiera@outlook.com>
2020-04-04 12:46:07 +0200

    Basic pane layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
 
+import { NgxResizableModule } from '@3dgenomes/ngx-resizable';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgxResizableModule],
   providers: [],
   bootstrap: [AppComponent],
 })
